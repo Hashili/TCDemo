d54e0e2cc9ba8ce9173cd498b46ce382cf21f673
Hashili Abdul <habdul@planittesting.com>
2019-05-02 08:30:59 +0800

    test sample

---
+++ src/main/java/Sample/DemoProject/MyDemoClass.java
@@ -0,0 +1,30 @@
+package Sample.DemoProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyDemoClass {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver  = null;
+		// TODO Auto-generated method stub
+		System.out.println("****************************************************");
+		System.out.println("*****************This is a DemoCLass****************");
+		System.out.println("****************************************************");
+		System.setProperty("webdriver.driver.chrome", ".\\chromedriver.exe");
+		driver  = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://executeautomation.com/demosite/Login.html");
+		driver.findElement(By.name("UserName")).sendKeys("SomeUserName");
+		driver.findElement(By.name("Password")).sendKeys("SomePassword");
+		driver.findElement(By.xpath(("//input[@name='Login']"))).click();
+		Thread.sleep(5000);
+		driver.quit();
+
+	}
+
+}
